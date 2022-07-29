d779158b023f4aae26e5f27ab0d58dc9cbc68370
Sean Enright <seanrenright@gmail.com>
2022-07-28 22:39:25 -0700

    cleanup and handle multiple digits

--- final-project/t1/Parse.hs
+++ final-project/t1/Parse.hs
@@ -1,23 +1,22 @@
 module Parse where
---import Control.Monad.Fail
+
 import Control.Applicative hiding ((<|>))
 import Control.Monad (liftM, ap)
 import Debug.Trace ( traceM )
-import Data.Char ( isSpace )
+import Data.Char ( isSpace, isDigit, digitToInt )
 
 data Derivs = Derivs {
                -- Expressions
                dvAdditive :: Result Int,
                dvMultitive :: Result Int,
                dvPrimary :: Result Int,
-               dvDecimal :: Result Int,
+               dvInteger :: Result Int,
+               dvMultipleDigits :: Result (Int, Int),
+               dvSingleDigit :: Result Int,
 
                -- Lexical tokens
-               --dvDigits :: Result (Int, Int),
-               --dvDigit :: Result Int,
                dvSymbol :: Result Char,
-               dvSpacing :: Result (),
-               --dvWhitespace :: Result (),
+               dvWhitespace :: Result (),
 
                -- Raw input
                dvChar :: Result Char
@@ -42,14 +41,13 @@ instance Monad Parser where
             post (Parsed v d') = p' d'
                where Parser p' = f v
             post NoParse = NoParse
-   return x = Parser (\d -> Parsed x d)
+   return x = Parser (Parsed x)
 
 instance MonadFail Parser where
-   fail _ = Parser (\d -> NoParse)
+   fail _ = Parser (const NoParse)
 
 --instance Alternative Parser where
 --   empty = NoParse
---
 
 (<|>) :: Parser v -> Parser v -> Parser v
 (Parser p1) <|> (Parser p2) = Parser pre
@@ -60,82 +58,46 @@ instance MonadFail Parser where
 
 -- Main parse function
 -- Create a result matrix for an input string
-   --d = Derivs add mult prim dec digits digit sym whsp chr
 parse :: String -> Derivs
 parse s = d where
-   d = Derivs add mult prim dec sym spc chr
-   add = pAdditive d
-   mult = pMultitive d
-   prim = pPrimary d
-   dec = pDecimal d
-   sym = pSymbol d
-   spc = pSpacing d
-   --wht = pWhitespace d
-   {-
-   digits = pDigits d
-   digit = pDigit d
-   sym = pSymbol d
-   whsp = pWhitespace d
-   chr = pChar d
-   -}
-   chr = case s of
-           (c:s') -> Parsed c (parse s')
-           []     -> NoParse
+   d      = Derivs add mult prim int muldig sindig sym spc chr
+   add    = pAdditive d
+   mult   = pMultitive d
+   prim   = pPrimary d
+   int    = pInteger d
+   muldig = pMultipleDigits d
+   sindig = pSingleDigit d
+   sym    = pSymbol d
+   spc    = pWhitespace d
+   chr    = case s of
+               (c:s') -> Parsed c (parse s')
+               []     -> NoParse
 
 
----------- Character-oriented parsers
+-- vvv Begin parsing functions for non-terminals
 
-
-{-
--- TODO: figure out this char function
-char :: Derivs -> Result Char
-char c = if c == '+'
-             then Parsed c d
-             else NoParse
--}
-
--- "Parser combinator" to look for a specific symbol.
--- Cannot be memoized directly because it takes a parameter,
--- but that does not matter because it is non-recursive.
-{-
-symbol c =
-   do c' <- Parser dvSymbol
-      if c' == c then return c
-         else fail []
-   -- <?> show c
--}
-
-symbolz c =
-   do c' <- Parser dvChar
-      traceM $ "symbol: " ++ [c] ++ ", stream: " ++ [c']
-      if c' == c then 
-         do traceM "success"
-            return c'
-      else fail []
-   
-symbol c =
-   do c' <- Parser dvSymbol
-      if c' == c then return c
-         else fail []
-
+pAdditive :: Derivs -> Result Int
 Parser pAdditive =
    do traceM "Starting additive"
+      Parser dvWhitespace
       vleft <- Parser dvMultitive
       traceM "Before plus"
       symbol '+'
       traceM "After plus"
       vright <- Parser dvAdditive
-      return (vleft + vright)
-   <|> (do Parser dvMultitive)
+      return $ vleft + vright
+   <|> do Parser dvMultitive
 
+pMultitive :: Derivs -> Result Int
 Parser pMultitive =
    do traceM "Starting multitive"
       vleft <- Parser dvPrimary
       symbol '*'
       vright <- Parser dvMultitive
-      return (vleft * vright)
-      <|> (do Parser dvPrimary)
+      return $ vleft * vright
+   <|> do Parser dvPrimary
 
+pPrimary :: Derivs -> Result Int
 Parser pPrimary =
    do traceM "starting pPrimary"
       symbol '('
@@ -145,83 +107,74 @@ Parser pPrimary =
       symbol ')'
       traceM "after rparen"
       return vleft
-      <|> (do Parser dvDecimal)
+   <|> do Parser dvInteger
 
-Parser pDecimal =
-   do symbol '0'
-      return 0
-   <|>
-   do symbol '1'
-      return 1
-   <|>
-   do symbol '2'
-      return 2
-   <|>
-   do symbol '3'
-      return 3
-   <|>
-   do symbol '4'
-      return 4
-   <|>
-   do symbol '5'
-      return 5
-   <|>
-   do symbol '6'
-      return 6
-   <|>
-   do symbol '7'
-      return 7
-   <|>
-   do symbol '8'
-      return 8
-   <|>
-   do symbol '9'
-      return 9
-   
-      
-Parser pDigits = undefined
-Parser pDigit = undefined
+pInteger :: Derivs -> Result Int
+Parser pInteger =
+   do (v,n) <- Parser dvMultipleDigits
+      Parser dvWhitespace
+      return v
 
--- Parsing functions for lexical analysis
-anyChar :: Parser Char
-anyChar = Parser dvChar
+pMultipleDigits :: Derivs -> Result (Int, Int)
+Parser pMultipleDigits =
+   do v <- Parser dvSingleDigit
+      (v',n) <- Parser dvMultipleDigits
+      return (v * 10^n + v', n + 1)
+   <|>
+   do v <- Parser dvSingleDigit
+      return (v, 1)
 
-oneOf :: Foldable t => t Char -> Parser Char
-oneOf chs = satisfy anyChar (`elem` chs)
+pSingleDigit :: Derivs -> Result Int
+Parser pSingleDigit =
+   do digitToInt <$> digit
 
-satisfy :: Parser v -> (v -> Bool) -> Parser v
-satisfy (Parser p) predicate = Parser parse
-      where parse dvs = check dvs (p dvs)
-            check dvs result@(Parsed val rem) =
-               if predicate val then result
-               else NoParse
-            check dvs none = none
+
+-- ^^^ End parsing functions for non-terminals
+
+-- vvv Begin non-terminal parsing functions
 
 -- From Ford paper
 -- Parse an operator followed by optional whitespace
 pSymbol :: Derivs -> Result Char
 Parser pSymbol =
    do c <- oneOf "+-*/%()0123456789"
-      Parser dvSpacing
+      Parser dvWhitespace
       return c
 
-space = satisfy anyChar isSpace
-
 -- Parse zero or more whitespace characters
-Parser pSpacing =
-   do space
-      Parser dvSpacing
-      return ()
-   <|>
-   do return ()
-
--- From Ford paper
 pWhitespace :: Derivs -> Result ()
-pWhitespace d =
-   case dvChar d of
-      Parsed c d' ->
-         if isSpace c
-         then pWhitespace d'
-         else Parsed () d
-      _ -> Parsed () d
- 
+Parser pWhitespace =
+   do space
+      Parser dvWhitespace
+      return ()
+   <|> do return ()
+
+-- ^^^ End non-terminal parsing functions
+
+
+-- Helper functions for lexical analysis
+anyChar :: Parser Char
+anyChar = Parser dvChar
+
+oneOf :: Foldable t => t Char -> Parser Char
+oneOf chs = sat anyChar (`elem` chs)
+
+sat :: Parser v -> (v -> Bool) -> Parser v
+sat (Parser p) predicate = Parser parse
+      where parse dvs = check dvs (p dvs)
+            check dvs result@(Parsed val rem) =
+               if predicate val then result
+               else NoParse
+            check dvs none = none
+
+symbol :: Char -> Parser Char
+symbol c =
+   do c' <- Parser dvSymbol
+      if c' == c then return c
+         else fail []
+
+space :: Parser Char
+space = sat anyChar isSpace
+
+digit :: Parser Char
+digit = sat anyChar isDigit
